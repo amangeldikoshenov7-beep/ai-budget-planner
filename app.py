7147fc6fbf3f4b36089cc8a1582388cf6d8c6a47
amangeldikoshenov7-beep <amangeldikoshenov7@gmail.com>
2026-05-02 17:37:45 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,57 @@
+import streamlit as st
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+
+import joblib
+
+model = joblib.load("budget_forecast_model.pkl")
+
+st.title("💰 AI Budget Planner")
+
+uploaded_file = st.file_uploader("Загрузите CSV", type=["csv"])
+
+if uploaded_file is not None:
+
+    df = pd.read_csv(uploaded_file)
+    df['date'] = pd.to_datetime(df['date'])
+
+    st.write("### Данные")
+    st.write(df.head())
+
+    category_df = df.groupby(['date','category'])['amount_kzt'].sum().reset_index()
+    category_df = category_df.sort_values(['category','date'])
+
+    category_df['month'] = category_df['date'].dt.month
+    category_df['year'] = category_df['date'].dt.year
+
+    category_df['lag_1'] = category_df.groupby('category')['amount_kzt'].shift(1)
+    category_df['lag_2'] = category_df.groupby('category')['amount_kzt'].shift(2)
+    category_df['lag_3'] = category_df.groupby('category')['amount_kzt'].shift(3)
+
+    category_df = category_df.dropna()
+
+    X = category_df[['month','year','lag_1','lag_2','lag_3']]
+    y = category_df['amount_kzt']
+
+    train_size = int(len(X) * 0.8)
+
+    X_train = X[:train_size]
+    y_train = y[:train_size]
+
+    model = RandomForestRegressor(n_estimators=100)
+    model.fit(X_train, y_train)
+
+    st.success("Модель обучена")
+
+    last_data = category_df.groupby('category').last().reset_index()
+    last_data['month'] = last_data['month'] + 1
+
+    future_X = last_data[['month','year','lag_1','lag_2','lag_3']]
+    preds = model.predict(future_X)
+
+    last_data['prediction'] = preds
+
+    st.write("### Прогноз")
+    st.write(last_data[['category','prediction']])
+
+    st.line_chart(df.groupby('date')['amount_kzt'].sum())
